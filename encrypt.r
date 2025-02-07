c6f51b9f24658e1f23a7a635ca03cd0c16ff683e
Paul Schmidt <schmidtpaul@hotmail.de>
2025-02-07 15:07:08 +0100

    init

---
+++ encrypt.r
@@ -0,0 +1,10 @@
+staticryptR::staticryptr(
+  files = "docs/",        # Changed from _output to docs
+  directory = ".",
+  password = "pw",
+  short = TRUE,
+  recursive = TRUE,
+  template_title = "Your Website Title",
+  template_instructions = "Please enter the password to access this site",
+  template_button = "Access"
+)
