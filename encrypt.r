859c79fca53a7c71b1637d92e2ac7cad4db03993
Paul Schmidt <schmidtpaul@hotmail.de>
2025-02-07 15:20:25 +0100

    fix encrypt.r

--- encrypt.r
+++ encrypt.r
@@ -1,10 +1,7 @@
+# encrypt.r
 staticryptR::staticryptr(
-  files = "docs/",        # Changed from _output to docs
-  directory = ".",
-  password = "pw",
-  short = TRUE,
-  recursive = TRUE,
-  template_title = "Your Website Title",
-  template_instructions = "Please enter the password to access this site",
-  template_button = "Access"
+  files = c("docs/index.html", "docs/about.html"),
+  directory = "docs",
+  password = "yourverylongpassword",
+  short = TRUE
 )
