db7707d46dfc2a219147a5f766fd67612d56ee3b
Paul Schmidt <schmidtpaul@hotmail.de>
2025-02-07 16:57:36 +0100

    encrypt finally works?

--- encrypt.r
+++ encrypt.r
@@ -1,16 +1,24 @@
-staticryptR::staticryptr(
-  files = "docs/index.html",
-  directory = ".",
-  password = "yourverylongpassword",
-  short = TRUE,
-  print_cmd = FALSE
-)
+password <- "pw"
+html_files <- list.files("docs", pattern = "\\.html$", full.names = TRUE)
 
-staticryptR::staticryptr(
-  files = "docs/about.html",
-  directory = ".",
-  password = "yourverylongpassword",
-  short = TRUE,
-  print_cmd = FALSE
-)
-
+for (file in html_files) {
+  staticryptR::staticryptr(
+    files = file,
+    directory = ".",
+    password = password,
+    short = TRUE,
+    template_button = "Bestätigen",
+    template_color_primary = "#00923f",
+    template_color_secondary = "#bce2cc",
+    template_instructions = "Um auf den Inhalt des Workshops zugreifen zu können, müssen Sie sich erst einloggen. Einloggen können sich alle Personen, die zu Beginn des Workshops ein Passwort erhalten haben. Sollte es Probleme geben, kontaktieren Sie uns per E-Mail.",
+    template_error = "Falsches Passwort",
+    template_placeholder = "Passwort",
+    template_remember = "Passwort merken",
+    template_title = "Data Scientist - Focus Python",
+    template_toggle_hide = "template_toggle_hide",
+    template_toggle_show = "template_toggle_show"
+    
+  )
+  
+  cat(glue::glue("\n\n> Encrypted '{file}'"))
+}
